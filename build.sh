236e168542d16734bb73f27e1108b44ffbbd45c4
Budry <zarubaondra@gmail.com>
2017-04-01 18:51:11 +0200

    Added optional target tag

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 help() {
-  echo "Usage: ${0} <image> <tag>"
+  echo "Usage: ${0} <image> <tag> [<target tag>]"
 }
 
 if [ -z "$1" ] ; then
@@ -16,6 +16,12 @@ if [ -z "$2" ] ; then
         exit 1
 fi
 
+if [ -z "$3" ] ; then
+	TARGET_TAG=$2
+else
+	TARGET_TAG=$3
+fi
+
 BASEDIR="$(dirname $0)"
 
 echo "Run update.sh ${2}"
@@ -26,8 +32,8 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "Run docker build -t ${1}:${2} ${BASEDIR}"
-docker build -t ${1}:${2} ${BASEDIR}
+echo "Run docker build -t ${1}:${TARGET_TAG} ${BASEDIR}"
+docker build -t ${1}:${TARGET_TAG} ${BASEDIR}
 
 echo "Update image on docker HUB"
-docker push docker.io/${1}:${2}
+docker push docker.io/${1}:${TARGET_TAG}
