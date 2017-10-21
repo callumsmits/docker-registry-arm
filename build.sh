90b3d3d19b880625b1c72f069ec7c1598dc985bc
Budry <zarubaondra@gmail.com>
2017-10-21 20:36:02 +0200

    Updated build process

--- build.sh
+++ build.sh
@@ -1,39 +1,36 @@
 #!/usr/bin/env sh
 
 help() {
-  echo "Usage: ${0} <image> <tag> [<target tag>]"
+  echo "Usage: ${0} <image> [<tag> default from TAG file]"
 }
 
 if [ -z "$1" ] ; then
-        echo "Missing image name"
-        help
-        exit 1
+  echo "Missing image name"
+  help
+  exit 1
+else   
+    IMAGE_NAME=$1
 fi
 
 if [ -z "$2" ] ; then
-        echo "Missing image tag"
-        help
-        exit 1
+  TAG=$(cat TAG)
+else
+  TAG=$2
 fi
 
-if [ -z "$3" ] ; then
-	TARGET_TAG=$2
-else
-	TARGET_TAG=$3
-fi
 
 BASEDIR="$(dirname $0)"
 
 echo "Run update.sh ${2}"
-sh ${BASEDIR}/update.sh $2
+sh ${BASEDIR}/update.sh ${TAG}
 
 if [ $? -ne 0 ]; then
   echo "Build failed"
   exit 1
 fi
 
-echo "Run docker build -t ${1}:${TARGET_TAG} ${BASEDIR}"
-docker build -t ${1}:${TARGET_TAG} ${BASEDIR}
+echo "Run docker build -t ${IMAGE_NAME}:${TAG} ${BASEDIR}"
+docker build -t ${IMAGE_NAME}:${TAG} -t ${IMAGE_NAME}:latest ${BASEDIR}
 
 echo "Update image on docker HUB"
-docker push docker.io/${1}:${TARGET_TAG}
+docker push docker.io/${IMAGE_NAME}:${TAG}
